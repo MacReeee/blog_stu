bcfd988a3762c8dbd4e68607bafd88f9972f7792
yang <2645636106@qq.com>
2024-07-05 16:20:46 +0800

    尝试加入插入图片功能，但是失败了，跳过这部分功能

--- api/post.go
+++ api/post.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"github.com/qiniu/go-sdk/v7/auth/qbox"
+	"github.com/qiniu/go-sdk/v7/storage"
 	"errors"
 	"goblog/common"
+	"goblog/config"
 	"goblog/dao"
 	"goblog/models"
 	"goblog/service"
@@ -110,4 +113,16 @@ func (*Api) SaveAndUpdatePost(w http.ResponseWriter, r *http.Request) {
 		service.UodatePost(post)
 		common.Success(w, post)
 	}
+}
+
+func (*Api) QiniuToken(w http.ResponseWriter, r *http.Request) {
+	//自定义凭证有效期（示例2小时，Expires 单位为秒，为上传凭证的有效时间）
+	bucket := "zhangsan"
+	putPolicy := storage.PutPolicy{
+		Scope: bucket,
+	}
+	putPolicy.Expires = 7200 //示例2小时有效期
+	mac := qbox.NewMac(config.Cfg.System.QiniuAccessKey, config.Cfg.System.QiniuSecretKey)
+	upToken := putPolicy.UploadToken(mac)
+	common.Success(w, upToken)
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,16 @@ go 1.22.4
 
 require (
 	github.com/BurntSushi/toml v1.4.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/qiniu/go-sdk/v7 v7.21.1
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/alex-ant/gomath v0.0.0-20160516115720-89013a210a82 // indirect
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/matishsiao/goInfo v0.0.0-20210923090445-da2e3fa8d45f // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
